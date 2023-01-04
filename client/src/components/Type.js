6268068d2ff7e9ca85a6bde7e8ef60247f2a27ed
hyerimhan <hnhyerim@gmail.com>
2023-01-04 22:51:19 +0900

    fix: axios error not resolved

--- client/src/components/Type.js
+++ client/src/components/Type.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Type({ orderType }) {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    loadItems(orderType);
+  }, [orderType]);
+
+  const loadItems = async (orderType) => {
+    try {
+      let res = await axios.get(`http://localhost:3000/${orderType}`);
+      console.log(res);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <h2>주문 종류</h2>
