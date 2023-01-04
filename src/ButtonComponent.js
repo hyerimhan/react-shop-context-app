03b6ccdac517245f71e8da986cbd0afcf1a82d51
hyerimhan <hnhyerim@gmail.com>
2023-01-04 20:52:28 +0900

    feat: Start Context

---
+++ src/ButtonComponent.js
@@ -0,0 +1,41 @@
+import "./App.css";
+import React, { useContext } from "react";
+
+const themes = {
+  light: {
+    foreground: "#000",
+    background: "#eee",
+  },
+  dark: {
+    foreground: "#fff",
+    background: "#222",
+  },
+};
+
+const ThemeContext = React.createContext(themes.light);
+
+function App() {
+  return (
+    <ThemeContext.Provider value={themes.dark}>
+      <Toolbar />
+    </ThemeContext.Provider>
+  );
+}
+
+function Toolbar() {
+  return (
+    <div>
+      <ThemedButton />
+    </div>
+  );
+}
+
+function ThemedButton() {
+  const theme = useContext(ThemeContext);
+  return (
+    <button style={{ background: theme.background, color: theme.foreground }}>
+      컨텍스트에 의해 스타일이 변함
+    </button>
+  );
+}
+export default App;
